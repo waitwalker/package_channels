9e1380a81416c8fccdf9f0b0f857fe862d6fd27a
waitwalker <waitwalker@163.com>
2020-07-02 10:30:27 +0800

    初始化项目

---
+++ ios/Classes/PackageChannelsPlugin.m
@@ -0,0 +1,20 @@
+#import "PackageChannelsPlugin.h"
+
+@implementation PackageChannelsPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"package_channels"
+            binaryMessenger:[registrar messenger]];
+  PackageChannelsPlugin* instance = [[PackageChannelsPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
