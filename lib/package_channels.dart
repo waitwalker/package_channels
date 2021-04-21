09f90dfcaf1f05d9d40992e1e4b0ab26fd2f88a1
waitwalker <waitwalker@163.com>
2021-04-21 15:46:45 +0800

    适配null safety

--- lib/package_channels.dart
+++ lib/package_channels.dart
@@ -7,14 +7,14 @@ class PackageChannels {
   static const MethodChannel _channel =
       const MethodChannel('package_channels');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
+  static Future<String?> get platformVersion async {
+    final String? version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
 
-  static Future<String> get getChannel async {
+  static Future<String?> get getChannel async {
     if(Platform.isAndroid) {
-      final String version = await _channel.invokeMethod('getChannel');
+      final String? version = await _channel.invokeMethod('getChannel');
       return version;
     }
     return 'AppStore';
